c42833aa60d1fbcbac2dfdc22898d312fbbbcefc
QuickOrBeDead <boraakgn@gmail.com>
2022-12-26 19:45:24 +0300

    add projects

---
+++ RabbitMQ/RabbitMq/SearchHandler/Program.cs
@@ -0,0 +1,10 @@
+using SearchHandler;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
