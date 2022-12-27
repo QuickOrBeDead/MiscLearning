eda0ad97e8c48811d1b404aff44cd167cb7adce7
QuickOrBeDead <boraakgn@gmail.com>
2022-12-27 15:02:21 +0300

    add rabbitmq to docker compose

--- RabbitMQ/RabbitMq/SearchApi/Program.cs
+++ RabbitMQ/RabbitMq/SearchApi/Program.cs
@@ -16,7 +16,7 @@ app.MapGet("/search", (string q, IConnection connection) =>
     {
         using (var channel = connection.CreateModel())
         {
-            channel.QueueDeclare("Search", false, false, false, null);
+            channel.QueueDeclare("Search", true, false, false);
             channel.BasicPublish(
                 "",
                 "Search",
