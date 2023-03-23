1ca996b49a767874a4785abd339dd83e67d478cf
QuickOrBeDead <boraakgn@gmail.com>
2023-03-23 17:14:45 +0300

    remove wwwroot

--- OcelotIdentityServer/IdentityServer/Program.cs
+++ OcelotIdentityServer/IdentityServer/Program.cs
@@ -1,12 +1,8 @@
 using IdentityServer;
 
-using IdentityServer4.Test;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-
 builder.Services.AddIdentityServer()
     .AddInMemoryClients(IdentityServerConfig.Clients)
     .AddInMemoryApiScopes(IdentityServerConfig.ApiScopes)
@@ -18,13 +14,7 @@ builder.Services.AddIdentityServer()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
-app.UseStaticFiles();
 app.UseRouting();
 app.UseIdentityServer();
-app.UseAuthorization();
 
 app.Run();
