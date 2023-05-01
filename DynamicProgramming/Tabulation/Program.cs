b0579a89a7ac93430c81e7e7d6570885cb310eee
QuickOrBeDead <boraakgn@gmail.com>
2023-05-01 13:56:51 +0300

    add Tabulation project

---
+++ DynamicProgramming/Tabulation/Program.cs
@@ -0,0 +1,16 @@
+﻿const int q = 8;
+Console.WriteLine($"f({8}) = {Fibonacci(8)}");
+
+int Fibonacci(int n) 
+{
+    var lookup = new int[n + 1];
+    lookup[0] = 0;
+    lookup[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        lookup[i] = lookup[i - 1] + lookup[i - 2];
+    }
+
+    return lookup[n];
+}
