ad05dbe4c081524ce4e2ed961f72e8a5f0e95b2f
QuickOrBeDead <boraakgn@gmail.com>
2023-05-01 16:44:33 +0300

    add LongestIncreasingSubsequence project

---
+++ DynamicProgramming/LongestIncreasingSubsequence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+var sequence = new int[] { 10, 22, 9, 33, 21, 50, 41, 60 };
+var lookup = Enumerable.Repeat(1, sequence.Length).ToArray();
+
+for (int i = 0; i < sequence.Length; i++)
+{
+    for (int j = i + 1; j < sequence.Length; j++)
+    {
+        if (sequence[j] > sequence[i]) 
+        {
+            var s1 = lookup[i];
+            if (s1 >= lookup[j]) 
+            {
+                lookup[j] = s1 + 1;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Result: {lookup.Max(x => x)}");
