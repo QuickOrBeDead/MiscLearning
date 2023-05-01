cc9706e4fb620bb4b67b5fb450a38fb1a8965650
QuickOrBeDead <boraakgn@gmail.com>
2023-05-01 13:39:46 +0300

    add DynamicProgramming solution add Memoization project

---
+++ DynamicProgramming/Memoization/Program.cs
@@ -0,0 +1,23 @@
+﻿const int n = 8;
+
+int[] lookup = new int[n + 1];
+int Fibonacci(int n) 
+{
+    var v = lookup[n];
+    if (v != 0) {
+        return v;
+    }
+   
+    if (n < 2) 
+    {
+        return (lookup[n] = n);
+    }
+    else
+    {
+        return (lookup[n] = Fibonacci(n -1) + Fibonacci(n - 2));
+    }
+}
+
+Console.WriteLine($"f({n}) = {Fibonacci(n)}");
+
+
