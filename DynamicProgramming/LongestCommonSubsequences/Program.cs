2dc38c30f2aed76dc1eb7a9b85686f817ea5c610
QuickOrBeDead <boraakgn@gmail.com>
2023-05-03 22:20:19 +0300

    read out the LCS string

--- DynamicProgramming/LongestCommonSubsequences/Program.cs
+++ DynamicProgramming/LongestCommonSubsequences/Program.cs
@@ -1,6 +1,6 @@
 ﻿
-var s1 = "ABCDEFG";
-var s2 = "BCDGK";
+var s1 = "XMJYAUZ";
+var s2 = "MZJAWXUDSADSAF";
 
 var n = s1.Length;
 var m = s2.Length;
@@ -22,5 +22,31 @@ for (var i = 1; i <= n; i++)
     }
 }
 
-Console.WriteLine("LCS('{0}', '{1}') = {2}", s1, s2, lp[n, m]);
+var len = lp[n, m];
+var s3 = new char[len];
+for (var i = n; i > 0; i--)
+{
+    for (var j = m; j > 0; j--)
+    {
+        while (i > 0 && lp[i, j] == lp[i - 1, j])
+        {
+            i--;
+        }
+
+        while (j > 0 && lp[i, j] == lp[i, j - 1])
+        {
+            j--;
+        }
+
+        if (i > 0 && lp[i, j] > 0)
+        {
+            s3[--len] = s1[i - 1];
+
+            i--;
+        }
+    }
+}
+
+
+Console.WriteLine("LCS('{0}', '{1}') = '{2}'", s1, s2, new string(s3));
 Console.ReadLine();
