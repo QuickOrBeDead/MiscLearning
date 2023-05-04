c3d891933b713102f1458126328e86a46b2e1e50
QuickOrBeDead <boraakgn@gmail.com>
2023-05-04 22:01:06 +0300

    implement NaiveLCS

--- DynamicProgramming/LongestCommonSubsequences/NaiveLCS.cs
+++ DynamicProgramming/LongestCommonSubsequences/NaiveLCS.cs
@@ -2,20 +2,18 @@ namespace LongestCommonSubsequences;
 
 public static class NaiveLCS
 {
-    public static string FindString(string s1, string s2) 
+    public static string FindString(string? s1, string? s2) 
     {
-        var seq1 = FindSubsequences(s1);
+        if (s1 == null || s2 == null)
+        {
+            return string.Empty;
+        }
+
+        var seq1 = FindSubsequences(s1).OrderByDescending(x => x.Length).ToList();
         var seq2 = FindSubsequences(s2);
         
-        var maxLen = Math.Min(s1.Length, s2.Length);
-
         for (int i = 0; i < seq1.Count; i++)
         {
-            if (seq1.Count > maxLen) 
-            {
-                continue;
-            }
-
             var s = seq1[i];
             if (seq2.Contains(s)) 
             {
@@ -26,47 +24,20 @@ public static class NaiveLCS
         return string.Empty;
     }
 
-    private static IList<string> FindSubsequences(string s) 
+    private static IList<string> FindSubsequences(string s, string seq = "", int i = 0) 
     {
-        if (string.IsNullOrEmpty(s)) 
-        {
-            return new List<string>(0);
-        }
-
         var result = new List<string>();
-        for (int n = 1; n < s.Length; n++)
+        for (; i < s.Length; i++)
         {
-            var seq = string.Empty;
-            for (int i = 0; i < s.Length; i++)
+            var newSeq = seq + s[i];
+            result.Add(newSeq);
+
+            if (i < s.Length - 1)
             {
-                seq += s[i].ToString();
-                n--;
-
-                if (n == 0)
-                {
-                    result.Add(seq);
-                    seq = string.Empty;
-                    n++;
-                    continue;
-                }
-
-                for (int j = n; j < s.Length; j++)
-                {
-                    seq += s[j].ToString();
-                    n--;
-
-                    if (n == 0)
-                    {
-                        result.Add(seq);
-                        seq = string.Empty;
-                        n++;
-                        continue;
-                    }
-                }
+                result.AddRange(FindSubsequences(s, newSeq, i + 1));
             }
         }
 
-        result.Add(s);
         return result;
     }
 }
--- DynamicProgramming/LongestCommonSubsequences/Program.cs
+++ DynamicProgramming/LongestCommonSubsequences/Program.cs
@@ -1,8 +1,19 @@
-﻿using LongestCommonSubsequences;
+﻿using System.Diagnostics;
+
+using LongestCommonSubsequences;
 
 var s1 = "XMJYAUZ";
 var s2 = "MZJAWXU";
 
-Console.WriteLine("DynamicProgrammingLCS('{0}', '{1}') = '{2}'", s1, s2, DynamicProgrammingLCS.FindString(s1, s2));
-Console.WriteLine("NaiveLCS('{0}', '{1}') = '{2}'", s1, s2, NaiveLCS.FindString(s1, s2));
+Calculate(s1, s2, nameof(DynamicProgrammingLCS), () => DynamicProgrammingLCS.FindString(s1, s2));
+Calculate(s1, s2, nameof(NaiveLCS), () => NaiveLCS.FindString(s1, s2));
 Console.ReadLine();
+
+static void Calculate(string s1, string s2, string name, Func<string> findStringFunc)
+{
+    var sw = Stopwatch.StartNew();
+    var r = findStringFunc();
+    sw.Stop();
+
+    Console.WriteLine("{0}('{1}', '{2}') = '{3}' in {4} ms", name, s1, s2, r, sw.ElapsedMilliseconds);
+}
