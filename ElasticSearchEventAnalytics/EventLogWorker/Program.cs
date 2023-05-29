8b0ff5ca80f0f4081d66c57fe82b9eb46496b72d
QuickOrBeDead <boraakgn@gmail.com>
2023-05-29 08:59:52 +0300

    register consumer channel

--- ElasticSearchEventAnalytics/EventLogWorker/Program.cs
+++ ElasticSearchEventAnalytics/EventLogWorker/Program.cs
@@ -11,6 +11,9 @@ IHost host = Host.CreateDefaultBuilder(args)
             var factory = new ConnectionFactory { HostName = "rabbitmq", UserName = "guest", Password = "guest", Port = 5672 };
             return factory.CreateConnection();
         });
+        services.AddSingleton<IModel>(x => {
+            return x.GetRequiredService<IConnection>().CreateModel();
+        });
         services.AddSingleton<IElasticClient>(_ => new ElasticClient(new ConnectionSettings(new Uri("http://elasticsearch:9200"))));
         services.AddHostedService<Worker>();
     })
