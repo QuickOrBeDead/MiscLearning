b6c3b25375dac6b18e86b35802d2f681384788ed
QuickOrBeDead <boraakgn@gmail.com>
2023-05-29 09:09:55 +0300

    add @timestamp attribute

--- ElasticSearchEventAnalytics/EventLogWorker/Model/EventLog.cs
+++ ElasticSearchEventAnalytics/EventLogWorker/Model/EventLog.cs
@@ -1,9 +1,12 @@
+using Nest;
+
 namespace EventLogWorker.Model;
 
 public sealed class EventLog
 {
     public Guid EventId { get; set; }
 
+    [Date(Name = "@timestamp")]
     public DateTime CreateDate { get; set; }
 
     public string DocumentId { get; set; }
