f82533818fe88223e3a47d6137011678cca53eb2
QuickOrBeDead <boraakgn@gmail.com>
2023-06-04 19:02:17 +0300

    add DocumentEventLog

---
+++ ElasticSearchEventAnalytics/EventLogWorker/Model/DocumentEventLog.cs
@@ -0,0 +1,27 @@
+using Nest;
+
+namespace EventLogWorker.Model;
+
+public sealed class DocumentEventLog
+{
+    public Guid EventId { get; set; }
+
+    [Keyword]
+    public string DocumentId { get; set; }
+
+    public bool? IsPdfCreated { get; set; }
+
+    public bool? IsSmsSent { get; set; }
+
+    public bool? IsEmailSent { get; set; }
+
+    public bool? IsDocumentViewed { get; set; }
+
+    [Date(Name = "@timestamp")]
+    public DateTime CreateDate { get; set; }
+
+    public DocumentEventLog()
+    {
+        CreateDate = DateTime.Now;
+    }
+}
---
+++ ElasticSearchEventAnalytics/PdfWorker/Model/DocumentEventLog.cs
@@ -0,0 +1,23 @@
+namespace PdfWorker.Model;
+
+public sealed class DocumentEventLog
+{
+    public Guid EventId { get; set; }
+
+    public string DocumentId { get; set; }
+
+    public bool? IsPdfCreated { get; set; }
+
+    public bool? IsSmsSent { get; set; }
+
+    public bool? IsEmailSent { get; set; }
+
+    public bool? IsDocumentViewed { get; set; }
+
+    public DateTime CreateDate { get; set; }
+
+    public DocumentEventLog()
+    {
+        CreateDate = DateTime.Now;
+    }
+}
