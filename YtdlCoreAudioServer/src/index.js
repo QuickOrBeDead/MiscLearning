8caef60915630d73e44af215d64e4fc9c332dc50
QuickOrBeDead <boraakgn@gmail.com>
2023-07-18 15:30:47 +0300

    add ytdl-core-audio-server

---
+++ YtdlCoreAudioServer/src/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+
+app.use(cors());
+
+app.listen(4000, () => {
+    console.log('Server Works !!! At port 4000');
+});
+
+app.get('/info/:videoID/', (req, res) => {
+    try {
+      const id = (req?.params?.videoID || '').replace(/[^A-Za-z0-9_\-]/g, '');
+      ytdl
+        .getInfo(id)
+        .then(info => {
+          const formats = {};
+          info.formats
+            .filter(file => file.mimeType.startsWith('audio'))
+            .map(file => {
+              formats[file.mimeType.split(';')[0]] = file.url;
+            });
+  
+          res.send({
+            url: ytdl.chooseFormat(info.formats, { filter: 'audioonly' }).url,
+            formats,
+            author: info.videoDetails.author.name,
+            title: info.videoDetails.title,
+            description: info.videoDetails.description,
+            images: info.player_response.videoDetails.thumbnail.thumbnails,
+          });
+        })
+        .catch(e => {
+          console.log(e);
+          return res.status(400).send({
+            url: '',
+            author: '',
+            title: '',
+          });
+        });
+    } catch (e) {
+      res.status(500).send({
+        error: 'unexpected server error',
+        reason: '',
+      });
+    }
+  });
