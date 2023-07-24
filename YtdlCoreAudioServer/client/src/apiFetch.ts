dfc557beb3b8de8dfbe692608510f8f0b1a55f23
QuickOrBeDead <boraakgn@gmail.com>
2023-07-24 20:58:40 +0300

    handle non 200 errors while fetching the request

--- YtdlCoreAudioServer/client/src/apiFetch.ts
+++ YtdlCoreAudioServer/client/src/apiFetch.ts
@@ -2,7 +2,19 @@ export const apiFetch = {
     get: <TResponse>(url: string) => request<TResponse>(url)
 }
 
+class FetchError extends Error {
+   constructor(message: string) {
+    super(message)
+
+    this.name = "FetchError"
+   }
+}
+
 async function request<TResponse>(url: string, config?: RequestInit): Promise<TResponse> {
     const response = await fetch(url, config)
+    if (!response.ok) {
+        const info = await response.json()
+        throw new FetchError(info && info.reason ? info.reason : 'fetch error')
+    }
     return await response.json()
 }
