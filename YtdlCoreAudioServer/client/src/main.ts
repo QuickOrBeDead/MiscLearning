ed8f59020861f1cc1becba41a1d2d7e6351776a6
QuickOrBeDead <boraakgn@gmail.com>
2023-07-24 20:33:01 +0300

    handle audio fetch error

--- YtdlCoreAudioServer/client/src/main.ts
+++ YtdlCoreAudioServer/client/src/main.ts
@@ -4,5 +4,8 @@ import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import App from './App.vue'
+import miniToastr from 'mini-toastr'
+
+miniToastr.init()
 
 createApp(App).use(createPinia()).mount('#app')
