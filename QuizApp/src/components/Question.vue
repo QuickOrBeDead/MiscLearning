53c12827c129cb890b8eb57cb55155bdd93d2da2
QuickOrBeDead <boraakgn@gmail.com>
2023-08-29 00:28:38 +0300

    implement prev next buttons

--- QuizApp/src/components/Question.vue
+++ QuizApp/src/components/Question.vue
@@ -20,22 +20,62 @@ const quiz: Quiz = {
                   text: "Madrid",
                   isCorrect: false
                 }]
+    },
+    {
+      text: "1 + 1 = ?",
+      answers: [{
+                  text: "1",
+                  isCorrect: false
+                }, 
+                {
+                  text: "2",
+                  isCorrect: true
+                }, 
+                {
+                  text: "3",
+                  isCorrect: false
+                }]
     }
   ]
 }
 
 const title = ref<string>()
 const question = ref<Question>()
+const questionIndex = ref<number>(0)
 
 onMounted(() => {
   title.value = quiz.title
-  question.value = quiz.questions[0]
+  
+  loadQuestion()
 })
+
+function loadQuestion() {
+  const index = questionIndex.value
+  if (index >= 0 && index < quiz.questions.length) {
+    question.value = quiz.questions[index]
+  }
+}
 
 function onAnswerSelected(i: number) {
   const answers = question.value?.answers as Answer[]
   answers.forEach(x => x.isSelected = false)
   answers[i].isSelected = true
+}
+
+function next() {
+  if (questionIndex.value < quiz.questions.length - 1) {
+    questionIndex.value++
+  }
+
+  loadQuestion()
+}
+
+function prev() {
+  if (questionIndex.value > 0) {
+    questionIndex.value--
+  }
+
+  loadQuestion()
 }
 
 </script>
@@ -44,9 +84,9 @@ function onAnswerSelected(i: number) {
    <div class="container mt-5">
     <h1>{{ title }}</h1>
       <div class="mb-4">
-        <h3>Question 1: {{ question?.text }}</h3>
+        <h3>Question {{ questionIndex + 1 }}: {{ question?.text }}</h3>
         <div class="form-check" v-for="(answer, index) in question?.answers" :key="index">
-          <input class="answer-option-input form-check-input" type="radio" name="answer" :id="'a' + index" :value="index" @change="onAnswerSelected(index)">
+          <input class="answer-option-input form-check-input" type="radio" name="answer" :id="'a' + index" :value="index" :checked="answer.isSelected" @change="onAnswerSelected(index)">
           <label class="answer-option-label form-check-label" :for="'a' + index">
             {{ answer.text }}
           </label>
@@ -54,8 +94,8 @@ function onAnswerSelected(i: number) {
       </div>
       
       <div class="btn-container">
-        <button type="button" class="btn btn-primary">Previous</button>
-        <button type="button" class="btn btn-primary">Next</button>
+        <button type="button" class="btn btn-primary" :disabled="questionIndex === 0" @click="prev">Previous</button>
+        <button type="button" class="btn btn-primary" :disabled="questionIndex === quiz.questions.length - 1" @click="next">Next</button>
       </div>
   </div>
 </template>
