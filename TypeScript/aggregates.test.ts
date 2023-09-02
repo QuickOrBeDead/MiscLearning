036478c25f4cdac74ae5b0c30836e12b9f16733a
QuickOrBeDead <boraakgn@gmail.com>
2023-09-02 16:18:51 +0300

    add count and avg and tests

--- TypeScript/aggregates.test.ts
+++ TypeScript/aggregates.test.ts
@@ -1,23 +1,102 @@
 import { expect, test } from '@jest/globals'
 import './aggregates'
 
+interface OrderItem {
+    name: string
 
-test('sum number array with map function', () => {
+    price: number
+}
+
+test('sum number array', () => {
     const arr = [1, 2, 3, 4, 5]
     expect(arr.sum(x => x)).toBe(15)
 })
 
-test('sum number array without map function', () => {
-    const arr = [10, 11, 12]
-    expect(arr.sum()).toBe(33)
-})
-
-test('sum number empty array with map function', () => {
+test('sum number empty array', () => {
     const arr = []
     expect(arr.sum(x => x)).toBe(0)
 })
 
-test('sum number empty array without map function', () => {
+test('sum OrderItem array', () => {
+    const arr: OrderItem[] = [{
+        name: 'Product 1',
+        price: 30.49
+    }, {
+        name: 'Product 2',
+        price: 99.99
+    }, {
+        name: 'Product 3',
+        price: 9.99
+    }]
+
+    expect(arr.sum(x => x.price)).toBe(140.47)
+})
+
+test('sum empty OrderItem array', () => {
+    const arr: OrderItem[] = []
+
+    expect(arr.sum(x => x.price)).toBe(0)
+})
+
+
+
+test('count number array', () => {
+    const arr = [1, 2, 3, 4, 5]
+    expect(arr.count()).toBe(5)
+})
+
+test('count number empty array', () => {
     const arr = []
-    expect(arr.sum()).toBe(0)
+    expect(arr.count()).toBe(0)
+})
+
+test('count OrderItem array', () => {
+    const arr: OrderItem[] = [{
+        name: 'Product 1',
+        price: 30.49
+    }, {
+        name: 'Product 2',
+        price: 99.99
+    }, {
+        name: 'Product 3',
+        price: 9.99
+    }]
+
+    expect(arr.count()).toBe(3)
+})
+
+test('count empty OrderItem array', () => {
+    const arr: OrderItem[] = []
+
+    expect(arr.count()).toBe(0)
+})
+
+
+
+test('avg number array', () => {
+    const arr = [1, 2, 3, 4, 5]
+    expect(arr.avg(x => x)).toBe(3)
+})
+
+test('avg number empty array', () => {
+    const arr = []
+    expect(arr.avg(x => x)).toBe(0)
+})
+
+test('avg OrderItem array', () => {
+    const arr: OrderItem[] = [{
+        name: 'Product 1',
+        price: 30.49
+    }, {
+        name: 'Product 2',
+        price: 99.99
+    }]
+
+    expect(arr.avg(x => x.price)).toBe(65.24)
+})
+
+test('avg empty OrderItem array', () => {
+    const arr: OrderItem[] = []
+
+    expect(arr.avg(x => x.price)).toBe(0)
 })
--- TypeScript/aggregates.ts
+++ TypeScript/aggregates.ts
@@ -2,22 +2,24 @@ export {}
 
 declare global {
     interface Array<T> {
-        sum(mapFn?: (v: T) => number): number
+        sum(mapFn: (v: T) => number): number
+
+        avg(mapFn: (v: T) => number): number
 
         count(): number
     }
 }
 
-const sumReduceFn = (accumulator: number, currentValue: number): number => accumulator + currentValue
-
 Array.prototype.sum = function<T>(mapFn: (v: T) => number): number {
-    return (this as Array<T>).map(v => mapFn(v)).reduce(sumReduceFn, 0)
-}
-
-Array.prototype.sum = function<T extends number>(): number {
-    return (this as Array<T>).reduce(sumReduceFn, 0)
+    return (this as Array<T>).map(v => mapFn(v)).reduce((accumulator: number, currentValue: number): number => accumulator + currentValue, 0)
 }
 
 Array.prototype.count = function<T>(): number {
     return (this as Array<T>).length
+}
+
+Array.prototype.avg = function<T>(mapFn: (v: T) => number): number {
+    const arr = this as Array<T>
+    const count = arr.count()
+    return count === 0 ? 0 : arr.sum(mapFn) / count
 }
