02ed8a0f5b07585a74bf4eed3892666584fee5fa
QuickOrBeDead <boraakgn@gmail.com>
2023-09-08 22:25:41 +0300

    add groupIndex to group Correct checkbox id

--- QuizApp/src/components/Quiz.vue
+++ QuizApp/src/components/Quiz.vue
@@ -396,11 +396,11 @@ function exportQuiz() {
                   </div>
 
                   <h5>Groups</h5>
-                  <template v-for="(group, index) in (question?.optionsContainer as TemplatedOptionsContainer).groups">
+                  <template v-for="(group, groupIndex) in (question?.optionsContainer as TemplatedOptionsContainer).groups">
                     <h6>
-                      Group {{ index }}
+                      Group {{ groupIndex }}
 
-                      <button type="button" class="btn btn-secondary" @click="() => removeOptionGroup((question?.optionsContainer as TemplatedOptionsContainer).groups, index)">
+                      <button type="button" class="btn btn-secondary" @click="() => removeOptionGroup((question?.optionsContainer as TemplatedOptionsContainer).groups, groupIndex)">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"></path>
                           <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"></path>
@@ -414,8 +414,8 @@ function exportQuiz() {
                         </div>
                         <div class="col-sm-1">
                           <div class="form-check">
-                            <input type="checkbox" v-model="option.isCorrect" class="form-check-input" :id="`is-correct-${index}`">
-                            <label class="form-check-label" :for="`is-correct-${index}`">Correct</label>
+                            <input type="checkbox" v-model="option.isCorrect" class="form-check-input" :id="`is-correct-${groupIndex}-${index}`">
+                            <label class="form-check-label" :for="`is-correct-${groupIndex}-${index}`">Correct</label>
                           </div>
                         </div>
                         <div class="col-sm">
