baae66e082569903ef70aba67f83feaaab9f4b9d
QuickOrBeDead <boraakgn@gmail.com>
2023-09-12 23:40:53 +0300

    add keys array

--- VueEnvironmentVariablesDocker/substitute_environment_variables.sh
+++ VueEnvironmentVariablesDocker/substitute_environment_variables.sh
@@ -2,11 +2,20 @@
 
 ROOT_DIR=/app
 
+keys="VARIABLE1
+VARIABLE2"
+
 # Replace env vars in files served by NGINX
 for file in $ROOT_DIR/assets/config-*.js*;
 do
-  sed -i 's|__VARIABLE1__|'${VARIABLE1}'|g' $file
-  sed -i 's|__VARIABLE2__|'${VARIABLE2}'|g' $file
+  echo "Processing $file ...";
+  for key in $keys
+  do
+    value=$(eval echo \$$key)
+    echo "replace $key by $value"
+    sed -i 's|__'"$key"'__|'"$value"'|g' $file
+  done
 done
+
 # Starting NGINX
 nginx -g 'daemon off;'
